3b2ce9bbcb746d1f2994d43df40908d67fc2692d
Paul PROST <paul.prost@hotmail.fr>
2023-05-24 20:39:03 +0200

    adding testing and README

--- internal/transport/http/get_price.go
+++ internal/transport/http/get_price.go
@@ -12,9 +12,12 @@ import (
 )
 
 // HTTP handler for the get price endpoint
+// @Param date query string true "Date in the format YYYY-MM-DD-HH.MM.SS"
+// @Param brandID query int true "Brand ID"
+// @Param productID query int true "Product ID"
 func GetPrice(cmd usecase.GetPriceCmd) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		date := c.Query("date")
+		date := c.Query("applicationDate")
 		brandID, err := strconv.ParseInt(c.Query("brandID"), 10, 64)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid brandID. Please provide a valid integer value."})
@@ -30,11 +33,11 @@ func GetPrice(cmd usecase.GetPriceCmd) gin.HandlerFunc {
 		parsedDate, err := time.Parse("2006-01-02-15.04.00", date)
 
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format. Please use YYYY-MM-DD."})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format. Please use YYYY-MM-DD-HH.MM.SS"})
 			return
 		}
 
-		price, err := cmd(c.Request.Context(), parsedDate, productID, brandID)
+		price, err := cmd(parsedDate, productID, brandID)
 		if err != nil {
 			switch err {
 			case domain.ErrPriceNotFound:
