19b6264e5070bc049e9a5c7f1e294b3600d34ab3
Paul PROST <paul.prost@hotmail.fr>
2023-05-24 16:25:42 +0200

    Gin router setup

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"log"
+	"pricesAPI/internal/infrastructure"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initRoute(router *gin.Engine, inMemorydatabase *infrastructure.InMemoryDatabase) {
+	// router.GET("/price", )
+}
+
 func main() {
 	// Create an instance of the in-memory database
-	// db := infrastructure.NewInMemoryDatabase()
+	inMemorydatabase := infrastructure.NewInMemoryDatabase()
+
+	router := gin.Default()
+
+	initRoute(router, inMemorydatabase)
+
+	err := router.Run()
+	if err != nil {
+		log.Fatal("error while running the router")
+	}
 }
